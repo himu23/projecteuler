87f0a2704e02d4023500327669014bdc9beb3fb9
himu23 <hmshete12@gmail.com>
2024-05-12 15:58:27 +0530

    2&3

---
+++ 1-50/3_largestprimefactor.cpp
@@ -0,0 +1,14 @@
+//any number can have at max one prime factor which is larger then it square root
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    long long int n=600851475143;
+    long long int m=sqrt(n);
+    for(int i=2;i<=m;i++){
+        
+    }
+
+    return 0;
+}
