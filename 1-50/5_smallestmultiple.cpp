9d15ac322cde6edff5f7cbbd0bb1cdb5cabef7d0
himu23 <hmshete12@gmail.com>
2024-05-15 20:50:24 +0530

    4

---
+++ 1-50/5_smallestmultiple.cpp
@@ -0,0 +1,12 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+using namespace std;
+
+int32_t main() {
+    int n;
+    cin>>n;
+    
+    return 0;
+}
