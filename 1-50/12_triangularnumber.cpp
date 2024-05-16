4f4cc5d0ad98253f40cae38d87c4c40a8b7c0e48
himu23 <hmshete12@gmail.com>
2024-05-16 13:00:06 +0530

    8-11

---
+++ 1-50/12_triangularnumber.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define pb push_back
+using namespace std;
+
+int32_t main() {
+    int n;
+    cin>>n;
+    
+    return 0;
+}
