377ea59e8d46db8127c72dabf9e99a067ef650bc
himu23 <hmshete12@gmail.com>
2024-07-23 18:19:03 +0530

    commit

---
+++ 1-50/29_distinctpowers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
+    int k; k=1;
+    cin>>k;
+    while(k--){
+        int n; cin>>n;
+        
+    }
+    return 0;
+}
