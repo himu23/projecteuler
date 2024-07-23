767684d62e5c6b51457737a82736b2f1a5c837f7
himu23 <hmshete12@gmail.com>
2024-07-23 23:00:18 +0530

    commit

--- 1-50/29_distinctpowers.cpp
+++ 1-50/29_distinctpowers.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 int main() {
     cin.tie(0)->sync_with_stdio(false);
-    int k; k=1;
-    cin>>k;
-    while(k--){
-        int n; cin>>n;
-        
+    int n; cin>>n;
+    set<double>s;
+    for(int i=2;i<=n;i++){
+        for(int j=2;j<=n;j++){
+            //double res=i*log10(j);
+            s.insert(pow(i,j));
+        }
     }
+    cout<<s.size()<<endl;
     return 0;
 }
